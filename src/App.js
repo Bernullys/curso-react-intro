9581c0425e8847a7332769e30a746d5be2cd48ff
badr <bernardoantoniod@gmail.com>
2023-08-28 20:59:11 -0400

    primer componente

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+          Editaaaaaaaaaaaa el archivo <code>src/App.js</code> y guarda para recargarrrrrrrrrrrr.
         </p>
         <a
           className="App-link"
@@ -15,10 +20,20 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React !
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>A llorar al valle</p>
+      <span>X</span>
+    </li>
   );
 }
 
